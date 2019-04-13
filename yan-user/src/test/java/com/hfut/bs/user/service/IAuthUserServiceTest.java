3cb835669b512e8cf1fe8843a8e05606d1c16fe5
dongju <954069680@qq.com>
2019-04-13 04:38:59 -0700

    基于dubbo分分布式学习平台完成用户服务、帖子服务，代码上传

---
+++ yan-user/src/test/java/com/hfut/bs/user/service/IAuthUserServiceTest.java
@@ -0,0 +1,12 @@
+package com.hfut.bs.user.service;
+
+public class IAuthUserServiceTest {
+
+
+    public static void main(String args[]){
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        IAuthUserService iAuthUserService =(IAuthUserService) context.getBean("authUserService");
+//        iAuthUserService.getByUsername("dongju");
+    }
+
+}
